c8c4557e1c85b20b78472dae5dab3658d7cca7cd
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2025-02-01 16:43:40 -0300

    Delete unused argument

--- server/config/index.ts
+++ server/config/index.ts
@@ -5,7 +5,6 @@ if (
   !process.env.CLIENT_ORIGIN_URL ||
   !process.env.MONGO_URI ||
   !process.env.PORT ||
-  !process.env.API_BASE_URL
 ) {
   console.error('Missing required arguments');
   process.exit(1);
@@ -14,5 +13,4 @@ if (
 export const PORT = process.env.PORT;
 export const CLIENT_ORIGIN_URL = process.env.CLIENT_ORIGIN_URL;
 export const MONGO_URI = process.env.MONGO_URI;
-export const API_BASE_URL = process.env.API_BASE_URL;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
