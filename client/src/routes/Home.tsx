dc187bd9f14a7f9ec43c4001888f5acd223126ce
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2025-02-10 12:38:49 -0300

    Style error and loading messages

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -5,10 +5,11 @@ import TaskForm from '../components/TaskForm';
 const Home = () => {
   const { error: fetchError, loading } = useTaskContext(); // ✅ Error from fetching tasks
 
-  if (fetchError) return <p className="text-red-500">{fetchError}</p>;
+  if (fetchError)
+    return <p className="m-auto font-bold text-red-500">{fetchError}</p>;
   if (loading)
     return (
-      <p className="text-grey-500 animate-pulse text-center text-xl font-bold">
+      <p className="text-grey-500 m-auto animate-pulse text-xl font-bold">
         Loading...
       </p>
     );
